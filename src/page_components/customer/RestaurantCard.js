76f2eba6881cb0d86fded50427f8b5bccb1e9547
Anastasiia Huhtala <anastasiia.huh@gmail.com>
2021-11-17 12:10:49 +0200

    first commit

---
+++ src/page_components/customer/RestaurantCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './css_modules/RestaurantCard.module.css';
+
+export default function RestaurantCard()
+{
+    return (
+        <div className="restCard">
+            
+        </div>
+    )
+}
