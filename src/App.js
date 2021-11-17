cb6ae04ecd76febab794f3e41ab062c989b2fc0e
Riku <t9orri00@students.oamk.fi>
2021-11-17 15:31:10 +0200

    render working

--- src/App.js
+++ src/App.js
@@ -12,18 +12,7 @@ class App extends React.Component {
   {
     super(props);
     this.state = {
-      restaurants: [
-        {
-          "restaurant_name": "Mcdonalds",
-          "restaurant_address": "testroad123",
-          "restaurant_operating_hours": "10-18",
-          "restaurant_image": "imageURL",
-          "restaurant_email": "mcdonalds@mcdonalds.fi",
-          "restaurant_password": "wwefweifjweoifjwoiejf",
-          "restaurant_type": "1",
-          "restaurant_price_level": 3
-      }
-      ],
+      restaurants: [],
       productSearchString: ""
     }
   }
@@ -32,12 +21,13 @@ class App extends React.Component {
   axios.get('https://voulutora-backend.herokuapp.com/restaurants')
     .then(response => {
       this.setState({restaurants: response.restaurants})
+      console.log(response);
     })
     .catch(err => console.log(err));
 
   }
   render() {
-    let output = <BrowsePage />
+    let output = <BrowsePage restaurants={this.state.restaurants} />
     return (
       <>
         { output }
--- src/pages/customer/BrowsePage.js
+++ src/pages/customer/BrowsePage.js
@@ -3,17 +3,15 @@ import styles from './css_modules/BrowsePage.module.css'
 import NavigationBar from "../../page_components/customer/NavigationBar";
 import RestaurantCard from '../../page_components/customer/RestaurantCard';
 
-const BrowsePage = props => {
-    return(
-        <div className={styles.container}>
-            <NavigationBar/>
-            <div>
-        {
-          props.restaurants.map(restaurant => <RestaurantCard key={restaurant.restaurant_name} {...restaurant} /> )
-        }
-        </div>
-        </div>
-    )
-  }
-  
-  export default BrowsePage; 
+export default function BrowsePage(props) {
+
+  const restaurantlist = props.restaurants.map(restaurant => <RestaurantCard key={restaurant.id} {...restaurant} />)
+  return (
+    <div>
+      {
+        restaurantlist
+      }
+      
+    </div>
+  )
+}
