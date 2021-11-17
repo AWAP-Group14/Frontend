b8662448bd37728833514b9400a5653f84d34e01
Henry Korhonen <t0kohe04@students.oamk.fi>
2021-11-17 14:12:48 +0200

    Updated navigation bar related files

--- src/page_components/customer/NavigationBar.js
+++ src/page_components/customer/NavigationBar.js
@@ -1,10 +1,15 @@
 import React from "react";
+
 import styles from './css_modules/NavigationBar.module.css'
 
 const NavigationBar = props => {
     return(
-        <div className={styles.NavigationBar}>
-            <p>Hi, I am navigation bar!</p>
+        <div className={styles.navigationBar}>
+            <div className={styles.navigationBarHeader}>Volutora</div>
+            <div className={styles.navigationBarUserAccount}>
+                <div><button className={styles.navigationBarButton}>Sign In</button></div>
+                <div><button className={styles.navigationBarButton}>Sign Up</button></div>
+            </div>
         </div>
     )
   }
