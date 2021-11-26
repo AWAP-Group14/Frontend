0abfad36beee61ec059bb7b5b93827e3a17284b2
Henry Korhonen <t0kohe04@students.oamk.fi>
2021-11-26 11:25:40 +0200

    More work on integrating bootstrap framefork

--- src/pages/customer/RestaurantMenuPage.js
+++ src/pages/customer/RestaurantMenuPage.js
@@ -1,31 +1,41 @@
 import React from "react";
-import styles from './css_modules/RestaurantMenuPage.module.css'
+import styles from './css_modules/RestaurantMenuPage.module.scss'
+
 import NavigationBar from "../../page_components/customer/NavigationBar";
 import Footer from '../../page_components/customer/Footer';
 import RestaurantInfoBox from "../../page_components/customer/RestaurantInfoBox";
 import RestaurantMenuCategories from "../../page_components/customer/RestaurantMenuCategories";
 import RestaurantItemCard from "../../page_components/shared/RestaurantItemCard";
-//import Button from 'react-bootstrap/Button';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default function RestaurantMenuPage(props) 
 {
     return(        
         <div className={styles.root}>
             <NavigationBar/>
+
             <div className={styles.restaurantHeader}>
                     <img className={styles.restaurantHeaderImage} src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Restaurant_N%C3%A4sinneula.jpg"></img>
                 </div>
 
-            <div className={styles.restaurantContent}>
+            <Container className={styles.restaurantContent}>
+                <Row className="g-20">
 
 
 
-                <div className={styles.restaurantCategories}>
+                
+
+
+                <Col className={styles.restaurantCategories}>
                     <RestaurantMenuCategories/>
                    
-                </div>
+                </Col>
 
-                <div className={styles.restaurantMenuCards}>
+                <Col className={styles.restaurantMenuCards}>
                     <div><h3>Category Name 1</h3></div>
                     <RestaurantItemCard/>
                     <RestaurantItemCard/>
@@ -39,13 +49,14 @@ export default function RestaurantMenuPage(props)
                     <RestaurantItemCard/>
                     <RestaurantItemCard/>
 
-                </div>
+                </Col>
 
-                <div className={styles.restaurantInfo}>
+                <Col className={styles.restaurantInfo}>
                     <RestaurantInfoBox/>
-                </div>
+                </Col>
 
-            </div>
+                </Row>
+            </Container>
             <Footer/>
         </div>
     )
