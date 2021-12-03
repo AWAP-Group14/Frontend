d3b647ec102570ccf6a4ede00c9dac79f6df9dfc
Anastasiia Huhtala <anastasiia.huh@gmail.com>
2021-12-03 11:34:50 +0200

    order history

--- src/pages/customer/OrderHistory.js
+++ src/pages/customer/OrderHistory.js
@@ -2,17 +2,18 @@ import React from "react";
 import NavigationBar from "../../page_components/customer/NavigationBar";
 import Footer from '../../page_components/customer/Footer';
 import HistoryCard from '../../page_components/customer/HistoryCard';
+import{Row} from "react-bootstrap";
 
 export default function OrderHistory(props) 
 {
     return(
         <div >
             <NavigationBar/>
-            <div>
+            <Row className="g-3">
                 <h1>Your previous choices: </h1>
                 <HistoryCard />
                 <HistoryCard />
-            </div>
+            </Row>
 
             <Footer />
         </div>
