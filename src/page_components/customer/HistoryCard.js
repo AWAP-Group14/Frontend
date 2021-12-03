49cdef92e5997cef00b83e6d26a5f42f2fb1d1c8
Riku <t9orri00@students.oamk.fi>
2021-12-03 14:40:40 +0200

    data to historycard

--- src/page_components/customer/HistoryCard.js
+++ src/page_components/customer/HistoryCard.js
@@ -5,9 +5,10 @@ import jwt from 'jsonwebtoken';
 
 export default function HistoryCard (props)
 {
+    
 
     const [isLoading, setLoading] = useState(true);
-    const [history, setHistory] = useState();
+    const [history, setHistory] = useState([]);
 
     const decodedToken = jwt.decode(props.jwt)
     console.log(decodedToken+" from the historypage")
@@ -16,13 +17,13 @@ export default function HistoryCard (props)
         let path = "https://voulutora-backend.herokuapp.com/orders/history/"+decodedToken.userId
         axios.get(path)
         .then(response => {
-            setHistory(response.data[0].id)
+            setHistory(response.data[0])
             setLoading(false)
             console.log(response.data[0].id);
         })
         .catch(err => {
             console.log(err);
-            setHistory("You haven't made any orders yet")
+            
         })
     },[])
 
@@ -30,17 +31,30 @@ export default function HistoryCard (props)
         
     }
 
-    return (
+    if (history != null) {
+
+        let orderItems = JSON.parse(history.items)
+        console.log(orderItems[0].item_name); 
+        return(
         <div>
             <img></img>
             <h1>!{props.RestaurantName}</h1>
-            <p>!{history}</p>
-            <p>!{props.DishName}</p>
-            <p>!{props.TotalPrice}</p>
-            <p>!{props.deliveryStatus}</p>
+            <p>!{history.id}</p>
+            <p>!{orderItems[0].item_name}</p>
+            <p>!{history.delivery_address}</p>
+            <p>!{history.total_price}</p>
             <p>!{props.date}</p>
             
         </div>
-    )
+        )
+    }
+
+    if (history == null) {
+        return (
+                <h3>You haven't made any orders yet</h3>
+
+            )
+    }
+    
 
 }
