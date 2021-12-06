57434b5c871583b520a9733c96d2778803ac366c
Anastasiia Huhtala <anastasiia.huh@gmail.com>
2021-12-06 21:02:58 +0200

    manager dashboard & footer fixed

--- src/page_components/customer/Footer.js
+++ src/page_components/customer/Footer.js
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from './css_modules/Footer.module.scss';
 import { Link } from "react-router-dom";
+import 'react-bootstrap';
+
+import { FaRegCopyright } from "react-icons/fa";
 
 export default function Footer (props)
 {
     if (props.jwt == null) {
         
     return (
+        <div className="fixed-bottom">
         <footer className={styles.footerContainer}>
             <p className={styles.footerLogo}>LOGO</p>
-            <div><p>Contact us:</p>
-            <p>Yliopistokatu 9, Oulu</p>
-            <p>1234 4567 789</p>
-            <p>contact@voulutora.com</p></div>
             <Link to="/manager/login"><button className={styles.ManagerPage}>Restaurant mangement page</button></Link>
         </footer>
+        <div className={styles.footerCopyright}><FaRegCopyright/> Group 14 Adwanced Web Development Project 2021 OAMK</div>
+        </div>
     )}
 
     if (props.jwt != null) {
--- src/pages/manager/LandingPageManager.js
+++ src/pages/manager/LandingPageManager.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { useState } from "react";
 import NavigationBar from "../../page_components/customer/NavigationBar";
-import styles from './css_modules/LandingPageManager.module.css';
-import OrderCard from '../../page_components/manager/OrderCard'
+import Footer from "../../page_components/customer/Footer";
+import styles from './css_modules/LandingPageManager.module.scss';
+import OrderCard from '../../page_components/manager/OrderCard';
+import {Button} from 'react-bootstrap';
 
 export default function LandingPageManager(props)
 {
     return(
         <div className={styles.App}>
             <NavigationBar jwt={props.jwt} logout={props.logout}/>
-            <p>Orders</p>
+            <h1>Orders</h1>
             <OrderCard/>
+            <div className={styles.btnCont}>
+            <Button>Order history</Button>
+            <Button>Edit menu</Button>
+            </div>
+            <Footer/>
         </div>
     )
   }
