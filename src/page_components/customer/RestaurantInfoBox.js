f98eecf6d9872e34ac400366142ee43afabec841
micmeier <michaelazhari.meier@protonmail.ch>
2021-12-09 14:49:50 +0200

    changed cancel to reject in manager dashboard page

--- src/page_components/customer/RestaurantInfoBox.js
+++ src/page_components/customer/RestaurantInfoBox.js
@@ -10,13 +10,13 @@ export default function RestaurantInfoBox(props)
         <div className="">
             <h2>{props.restaurantInfo.name}</h2>
             <p>Restaurant type: {props.restaurantInfo.type}</p>
-            {
-            props.restaurantInfo.operating_hours.map((line) => 
             <div>
             <p>Operating hours:</p>
+            {
+            props.restaurantInfo.operating_hours.map((line) => 
             <p className="ms-2">{line}</p>
-            </div>
             )}
+            </div>
             <p>{props.restaurantInfo.address}</p>
             <p>{props.restaurantInfo.email}</p>
         </div>
